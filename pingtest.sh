ac25929e300367171a6726de303d55eb1d4f6255
djrarky <sadfsafw83wfwe@git.com>
2025-04-25 17:31:48 -0300

    simplify

--- pingtest.sh
+++ pingtest.sh
@@ -39,43 +39,10 @@ ping_hosts() {
 
     # If ping to $HOST1 fails, try $HOST2
     if [ "${counting:-0}" -eq 0 ]; then
-        counting=$(ping -o -s 0 -c 10 -W 1 "$HOST2" | grep 'received' | awk -F',' '{ print $2 }' | awk '{ print $1 }')
+        counting="$(ping -o -s 0 -c 10 -W 1 "$HOST2" | grep 'received' | awk -F',' '{ print $2 }' | awk '{ print $1 }')"
     fi
 
     echo $counting
-}
-
-# Function to reset interface
-attempt_reset() {
-    local INTERFACE=$1
-    local RESET_STATUS=0
- 
-    # soft reload of interface
-    RESET_OUTPUT=$(/usr/local/sbin/pfSctl -c "interface reload $INTERFACE")
- 
-    if echo "$RESET_OUTPUT" | grep -q "OK"; then
-        RESET_STATUS=1
-    else
-        # If dhclient fails, log the error
-        log_message "$(date +'%d/%m/%y - %H:%M:%S') Failed to reset interface $INTERFACE. Response: $RESET_OUTPUT"
-    fi
-
-    # attempt pings again
-    counting=$(ping_hosts)
- 
-    # If both pings fail
-    if [ "${counting:-0}" -eq 0 ]; then
-        # yanks interface and brings it up fresh
-        php -r "require_once('/etc/inc/interfaces.inc'); interface_bring_down('$INTERFACE'); interface_configure('$INTERFACE');"
- 
-        if [ $? -ne 0 ]; then
-            # If PHP fails to reset the interface, log the error
-            log_message "$(date +'%d/%m/%y - %H:%M:%S') Failed to reset interface $INTERFACE"
-        else
-            RESET_STATUS=1
-        fi
-    fi
-    echo $RESET_STATUS
 }
 
 # Rotate the log if it's over 500KB
@@ -85,7 +52,7 @@ attempt_reset() {
  
 # Current time
 currtime=$(date +%s)
- 
+
 # Get boot time from sysctl output (extract 'sec = ####' value)
 boottime=$(sysctl -n kern.boottime | sed 's/.*sec = \([0-9]*\),.*/\1/')
  
@@ -106,30 +73,24 @@ if [ $utime -gt 120 ]; then
         log_message "$(date +'%d/%m/%y - %H:%M:%S') All pings failed. Resetting interface $INTERFACE."
  
         # Attempt to reset interface
-        RESET_STATUS=$(attempt_reset "$INTERFACE")
+        /usr/local/sbin/pfSctl -c "interface reload \"$INTERFACE\""
  
-        if [ $RESET_STATUS -eq 0 ]; then
-            log_message "$(date +'%d/%m/%y - %H:%M:%S') Resetting interface failed. Rebooting..."
+        sleep 5  # Adding a delay to ensure the interface resets
+ 
+        # Retest after NIC reset
+        counting=$(ping_hosts)
+ 
+        if [ "${counting:-0}" -eq 0 ]; then
+            # Network down
+            # Save RRD data
+            log_message "$(date +'%d/%m/%y - %H:%M:%S') All pings failed. Rebooting..."
  
             /etc/rc.backup_rrd.sh
             reboot
         else
-            sleep 5  # Adding a delay to ensure the interface resets
- 
-            # Retest after NIC reset
-            counting=$(ping_hosts)
- 
-            if [ "${counting:-0}" -eq 0 ]; then
-                # Network down
-                # Save RRD data
-                log_message "$(date +'%d/%m/%y - %H:%M:%S') All pings failed. Rebooting..."
- 
-                /etc/rc.backup_rrd.sh
-                reboot
-            else
-                log_message "$(date +'%d/%m/%y - %H:%M:%S') Interface reset successful, pings successful."
-            fi
+            log_message "$(date +'%d/%m/%y - %H:%M:%S') Interface reset successful, pings successful."
         fi
+    fi
     else
         # Network up
         log_message "$(date +'%d/%m/%y - %H:%M:%S') Network is up. Pings successful."
